13857c27575e89b5dcb6d9c76bad1bb13c1e6314
Daniel Romero <daniel21dev@gmail.com>
2021-01-24 13:44:37 -0600

    make it better

--- server/config/config.js
+++ server/config/config.js
@@ -5,8 +5,20 @@
 
 process.env.PORT = process.env.PORT || 3000
 
+// ==============================
+//  Entorno
+// ==============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
+// ==============================
+//  Base de datos
+// ==============================
+let urlDB;
 
-
-
+if( process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe';
+}else{
+    urlDB = 'mongodb+srv://daniel21dev:Xqlk04ROFG8VpTcS@cluster0.4txoq.mongodb.net/cafe?retryWrites=true&w=majority';
+}
+process.env.URLDB = urlDB;
 
--- server/server.js
+++ server/server.js
@@ -1,51 +1,29 @@
 require('./config/config')
-
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 
 // middlewares  nota: todas las peticiones pasan por los middlewares
-
 app.use(bodyParser.urlencoded({extended: false}))
-
 app.use(bodyParser.json())
+// importacion de los endpoints
+app.use( require('./routes/usuario') )
 
 
-app.get('/usuario', (req,res)=>{
-    res.json('get Usuario')
-})
+// configuracion mongoose ( para que no de advertencias )
+mongoose.set('useFindAndModify', false);
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useCreateIndex', true);
+// en la url <mongodb:> es el protocolo
+// esta es la conexión a la base de datos 
+mongoose.connect(process.env.URLDB,(err, res)=>{
+    if( err ) throw err;
 
-app.post('/usuario', (req,res)=>{
-
-    let body = req.body
-
-    if( body.nombre === undefined ){
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario',
-            
-        })
-    }else{
-        res.json({
-            persona: body
-        })
-    }
-    
-})
-// parametro :id
-app.put('/usuario/:id', (req,res)=>{
-
-    let id = req.params.id
-
-    res.json({
-        id
-    })
-})
-
-app.delete('/usuario', (req,res)=>{
-    res.json('delete Usuario')
-})
-
+    console.log(`Base de datos ONLINE`);
+});
+// listener escuchando las peticiones
 app.listen(process.env.PORT,()=>{
     console.log('escuchando puerto: ', process.env.PORT);
 })
